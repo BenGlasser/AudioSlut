f7bc9953021e239996598de45cb4d0415ef99909
Ben Glasser <soul_trip@hotmail.com>
2012-09-23 00:28:01 -0700

    add playback device menu item

--- AudioSlut/Program.cs
+++ AudioSlut/Program.cs
@@ -1,25 +1,32 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
-
-
+using System.Management;
+using System.Collections;
 
 namespace AudioSlut
 {
-    public class SysTrayApp : Form
+    public class AudioSlut : Form
     {
+        public static ArrayList devices = new ArrayList();
         [STAThread]
         public static void Main()
         {
-            Application.Run(new SysTrayApp());
+            Application.Run(new AudioSlut());
         }
         private NotifyIcon trayIcon;
         private ContextMenu trayMenu;
-        public SysTrayApp()
+        public AudioSlut()
         {
-            // Create a simple tray menu with only one item.  
+        
+            // Create a simple tray menu .  
             trayMenu = new ContextMenu();
             trayMenu.MenuItems.Add("Exit", OnExit);
+
+            MenuItem playbackMenu = new MenuItem("Playback Devices");
+            playbackMenu.MenuItems.AddRange(deviceMenu());
+            trayMenu.MenuItems.Add(playbackMenu);
+        
             // Create a tray icon. In this example we use a  
             // standard system icon for simplicity, but you  
             // can of course use your own custom icon too. 
@@ -48,6 +55,57 @@ namespace AudioSlut
                 trayIcon.Dispose();
             }
             base.Dispose(isDisposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // AudioSlut
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "AudioSlut";
+            this.Load += new System.EventHandler(this.AudioSlut_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private void AudioSlut_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void getDevices()
+        {
+            ManagementObjectSearcher objSearcher = new ManagementObjectSearcher(
+                "SELECT * FROM Win32_SoundDevice");
+            ManagementObjectCollection objCollection = objSearcher.Get();
+            foreach (ManagementObject obj in objCollection)
+            {
+                foreach (PropertyData property in obj.Properties)
+                {
+                    Console.Out.WriteLine(String.Format("{0}:{1}", property.Name, property.Value));
+                    if (property.Name == "Name"){
+                        devices.Add(property.Value);
+                    }
+                }
+            }
+        }
+        private MenuItem[] deviceMenu()
+        {
+            getDevices();
+            int i = 0;
+            MenuItem[] deviceMenu = new MenuItem[devices.Count];
+            foreach(String device in devices)
+            {
+                deviceMenu[i] = new MenuItem(device);
+                ++i;
+            }
+            return deviceMenu;
+
+        }
+        private void setDevice()
+        {
+
         }
     }
 }
