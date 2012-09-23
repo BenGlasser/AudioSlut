6faef92b39e4007fdfcf51e0243c2f5b13ea599d
Ben Glasser <soul_trip@hotmail.com>
2012-09-22 20:59:25 -0700

    add tray icon

--- AudioSlut/Form1.cs
+++ AudioSlut/Form1.cs
@@ -9,11 +9,16 @@ using System.Windows.Forms;
 
 namespace AudioSlut
 {
-    public partial class Form1 : Form
+    public partial class Form1: Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- AudioSlut/Program.cs
+++ AudioSlut/Program.cs
@@ -1,21 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Drawing;
 using System.Windows.Forms;
+
+
 
 namespace AudioSlut
 {
-    static class Program
+    public class SysTrayApp : Form
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SysTrayApp());
+        }
+        private NotifyIcon trayIcon;
+        private ContextMenu trayMenu;
+        public SysTrayApp()
+        {
+            // Create a simple tray menu with only one item.  
+            trayMenu = new ContextMenu();
+            trayMenu.MenuItems.Add("Exit", OnExit);
+            // Create a tray icon. In this example we use a  
+            // standard system icon for simplicity, but you  
+            // can of course use your own custom icon too. 
+            trayIcon = new NotifyIcon();
+            trayIcon.Text = "AudioSlut";
+            trayIcon.Icon = new Icon("../../Resources/speaker.ico", 40, 40);
+            // Add menu to tray icon and show it.
+            trayIcon.ContextMenu = trayMenu;
+            trayIcon.Visible = true;
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false; // Hide form window.           
+            ShowInTaskbar = false; // Remove from taskbar. 
+            base.OnLoad(e);
+        }
+        private void OnExit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                // Release the icon resource.
+                trayIcon.Dispose();
+            }
+            base.Dispose(isDisposing);
         }
     }
 }
+
